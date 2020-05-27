8748e19a8516cfa555ef5e0af07c857e7b82b2a3
grassswwang <Grass0319>
2020-05-27 11:24:16 +0800

    整理代码，一定要添加NotificationChannel

--- app/src/main/java/com/grass/hellonotification/MainActivity.kt
+++ app/src/main/java/com/grass/hellonotification/MainActivity.kt
@@ -26,24 +26,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.notify_btn).setOnClickListener {
             showNotification(this@MainActivity)
-            var notiManager = NotificationManagerCompat.from(this@MainActivity)
-            notiManager.notify(System.currentTimeMillis().toInt(), createNotification(this@MainActivity, notificationManager = notiManager))
         }
-    }
-
-    fun showNotification(context: Context) {
-        Log.i("grass", CHANNEL_ID)
-        var builder = NotificationCompat.Builder(context, CALENDAR_ID!!)
-                .setSmallIcon(getSuitableSmallIcon())
-                .setContentTitle("My notification")
-                .setContentText("Much longer text that cannot fit one line...")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true)
-
-        var notificationId = System.currentTimeMillis().toInt()
-        var notification = builder.build()
-        var notifyManager = NotificationManagerCompat.from(this)
-        notifyManager.notify(notificationId, notification)
     }
 
     /**
@@ -58,45 +41,54 @@ class MainActivity : AppCompatActivity() {
             R.mipmap.emui_icon
         } else R.mipmap.icon_white
         return R.drawable.kb_icon;
-//        return R.mipmap.ic_launcher_round
     }
 
     var CALENDAR_ID: String? = "channel_01"
 
-    private fun createNotification(context: Context, notificationManager: NotificationManagerCompat): Notification {
+    private fun showNotification(context: Context) {
+        var notificationManager = NotificationManagerCompat.from(this@MainActivity)
         val notification: Notification
         val builder: NotificationCompat.Builder
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(CALENDAR_ID, "123",
-                    NotificationManager.IMPORTANCE_DEFAULT)
-            // 设置渠道描述
-            notificationChannel.description = "测试通知组"
-            // 是否绕过请勿打扰模式
-            notificationChannel.canBypassDnd()
-            // 设置绕过请勿打扰模式
-            notificationChannel.setBypassDnd(true)
-            // 桌面Launcher的消息角标
-            notificationChannel.canShowBadge()
-            // 设置显示桌面Launcher的消息角标
-            notificationChannel.setShowBadge(true)
-            // 设置通知出现时声音，默认通知是有声音的
-            notificationChannel.setSound(null, null)
-            // 设置通知出现时的闪灯（如果 android 设备支持的话）
-            notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.RED
-            // 设置通知出现时的震动（如果 android 设备支持的话）
-            notificationChannel.enableVibration(true)
-            notificationChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400,
-                    300, 200, 400)
-            notificationManager.createNotificationChannel(notificationChannel)
+            notificationManager.createNotificationChannel(makeNotificationChannel())
         }
         builder = NotificationCompat.Builder(context, CALENDAR_ID!!)
         val pendingIntent = PendingIntent.getActivity(context, 11,
                 Intent(context, SecondActivity::class.java), 0)
-        notification = builder.setSmallIcon(R.mipmap.ic_launcher_round)
+        notification = builder.setSmallIcon(getSuitableSmallIcon())
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
                 .build()
-        return notification
+        notificationManager.notify(System.currentTimeMillis().toInt(), notification)
+    }
+
+    private fun makeNotificationChannel(): NotificationChannel {
+        val notificationChannel = NotificationChannel(CALENDAR_ID, "123",
+                NotificationManager.IMPORTANCE_DEFAULT)
+        // 设置渠道描述
+        notificationChannel.description = "测试通知组"
+        // 是否绕过请勿打扰模式
+        notificationChannel.canBypassDnd()
+        // 设置绕过请勿打扰模式
+        notificationChannel.setBypassDnd(true)
+        // 桌面Launcher的消息角标
+        notificationChannel.canShowBadge()
+        // 设置显示桌面Launcher的消息角标
+        notificationChannel.setShowBadge(true)
+        // 设置通知出现时声音，默认通知是有声音的
+        notificationChannel.setSound(null, null)
+        // 设置通知出现时的闪灯（如果 android 设备支持的话）
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor = Color.RED
+        // 设置通知出现时的震动（如果 android 设备支持的话）
+        notificationChannel.enableVibration(true)
+        notificationChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400,
+                300, 200, 400)
+        return notificationChannel;
     }
 
 }
